b91f32c7bd83a505916ee4fcd56ce49547dfde1f
ksamanasesh <manasesh2003@gmail.com>
2025-02-02 15:04:06 +0530

    ignore

--- RPS/settings.py
+++ RPS/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&s^v8(=eptjap@85^r-l^r$yy60u^glf%)ji$j86a9kcpn8qt%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,11 +79,11 @@ WSGI_APPLICATION = 'RPS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'UWidrltfzzpRflVEmpYYdQgbxsYmKaMl',
-        'HOST': 'autorack.proxy.rlwy.net',  
-        'PORT': '22532',  
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),  
+        'PORT': config('PORT'),  
     }
 }
 
